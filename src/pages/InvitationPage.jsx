d538ed8e989cddf5c73ef1b7e259404af6daf1ef
Rayane <alirayane14@gmail.com>
2023-07-29 10:16:12 +0200

    "Invitation page et search page"

--- src/pages/InvitationPage.jsx
+++ src/pages/InvitationPage.jsx
@@ -1,3 +1,90 @@
-function InvitationPage() {}
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import myaxios from "../myaxios";
+import NavBar from "../components/NavBar";
+
+function InvitationPage() {
+  const [email, setEmail] = useState("");
+  const { user } = useContext(AuthContext);
+
+  const handleEmailInput = (e) => setEmail(e.target.value);
+
+  const sendInvitations = () => {
+    // Créer un tab avec l'adresse email saisi par le user
+    const invitedUsers = [email];
+
+    // Vérifier que l'email n'est pas vide
+    if (invitedUsers[0] === "") {
+      alert("Please enter a valid email address");
+      return;
+    }
+
+    // Préparer le payload JSON pour L'API Brevo
+    const payload = {
+      sender: {
+        email: user.email,
+        name: user.username,
+      },
+      subject: "Invitation to join our group",
+      messageVersions: [
+        {
+          to: invitedUsers,
+          htmlContent:
+            "<!DOCTYPE html><html><body><h1>Welcome to our group!</h1><p>You are invited to join our group.</p></body></html>",
+          subject: "Invitation to join our group",
+        },
+      ],
+    };
+
+    // Appeler l'API Brevo pour envoyer les invitations par email
+    myaxios
+      .post("https://api.brevo.com/v3/smtp/email", payload, {
+        headers: {
+          accept: "application/json",
+          "api-key":
+            "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",
+          "content-type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Invitations sent successfully:", response.data);
+        alert(`Invitations sent successfully : ${response.data}`);
+      })
+      .catch((error) => {
+        console.error("Failed to send invitations:", error);
+        alert(`Failed to send invitations : ${error.response.data.message}`);
+      });
+  };
+
+  return (
+    <div className="invitationPage">
+      <img
+        className="profilePic"
+        src="../../images/profile.png"
+        alt="profile picture"
+      />
+      <form>
+        <div className="invitation">
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleEmailInput}
+          />
+          <button>Validate</button>
+        </div>
+
+        <div className="list-container">
+          <h4>List of people to invite</h4>
+        </div>
+        <button className="invitation-btn" onClick={sendInvitations}>
+          Send invitations
+        </button>
+      </form>
+
+      <NavBar />
+    </div>
+  );
+}
 
 export default InvitationPage;
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,3 +1,89 @@
-function SearchPage() {}
+import { useState, useEffect } from "react";
+import myaxios from "../myaxios";
+import NavBar from "../components/NavBar";
+import { Link } from "react-router-dom";
+
+function SearchPage() {
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [member, setMember] = useState("");
+  // State pour stocker les résultats de la recherche
+  const [searchResults, setSearchResults] = useState([]);
+  const [group, setGroup] = useState(null);
+
+  // Fonction pour MAJ le state member selon la valeur entrée dans la barre de recherche
+  const handleMemberInput = (e) => setMember(e.target.value);
+
+  useEffect(() => {
+    // Si le user a entré qqe chose dans la barre de rech
+    // Effectuer une  requête pour rechercher les membres du groupe en fonction du username entré
+    if (member) {
+      myaxios
+        .get(`/api/users/search?username=${member}`)
+        .then((response) => {
+          console.log("Search results:", response.data);
+          // MAJ des résultats de la recherche
+          setSearchResults(response.data);
+        })
+        .catch((error) => {
+          const errorDescription = error.response.data.message;
+          setErrorMessage(errorDescription);
+        });
+    } else {
+      // Si la barre de recherche est vide réinitilialiser les résultats de la recherche
+      setSearchResults([]);
+    }
+
+    myaxios
+      .get(`/api/group/me`)
+      .then((response) => {
+        // console.log("response.data groupe", response.data);
+
+        // MAJ des infos du groupe
+        setGroup(response.data);
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  }, [member]);
+
+  // Si le groupe n'est pas encore chargé afficher "loading..."
+  if (!group) {
+    return "loading...";
+  }
+
+  return (
+    <div className="searchPage">
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <h1>{group.name}</h1>
+      <div className="searchBar">
+        <label>
+          <img src="../../images/search-members.png" />
+          <input
+            id="member-search"
+            type="text"
+            name="search"
+            value={member}
+            onChange={handleMemberInput}
+          />
+          <button>Cancel</button>
+        </label>
+      </div>
+      <div className="searchResults">
+        {searchResults.map((user) => {
+          return (
+            <div key={user._id} className="searchResult">
+              <img src={user.profile_img} alt="profile picture" />
+              <Link to={`/profile/${user._id}`}>{user.username}</Link>
+            </div>
+          );
+        })}
+      </div>
+
+      <NavBar />
+    </div>
+  );
+}
 
 export default SearchPage;
