1e23fd96dcb126804b46ad862b6f68690c949253
Nicolas_M <nicolas.meot@live.fr>
2023-08-03 19:50:10 +0200

    modified the route for img upload

--- src/pages/AddPostPage.jsx
+++ src/pages/AddPostPage.jsx
@@ -1,23 +1,58 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import myaxios from "../myaxios";
 import { useNavigate } from "react-router-dom";
+import service from "../services/file-upload.service"
 
 function AddPostPage() {
   const [legend, setLegend] = useState("");
-  const [media, setMedia] = useState("");
+  const [mediasUrl, setMediasUrl] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const navigate = useNavigate();
 
+  const uploadImage = (files) => {
+    return myaxios
+      .post("/api/upload", files)
+      .then((res) => {
+        console.log("res.data ===", res.data);
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  
+
+  const handleFilesUpload = (e) => {
+    console.log("The files to be uploaded are: ", e.target.files);
+    // const files = e.target.files.FileList;
+    // console.log(files)
+    const uploadDatas = new FormData();
+    // files.forEach((file, i) => {
+    //   uploadDatas.append(`file-${i}`, file, file.name);
+    // });
+    // mediasUrl => this name has to be the same as in the model since we pass
+    // req.body to .create() method when creating a new movie in '/api/movies' POST route
+    uploadDatas.append("mediasUrl", e.target.files[0]);
+    uploadImage(uploadDatas)
+      .then(response => {
+        console.log("response is: ", response);
+        // response carries "fileUrl" which we can use to update the state
+        setMediasUrl(response.filesUrl);
+      })
+      .catch(err => console.log("Error while uploading the file: ", err));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const reqBody = { legend };
+    const reqBody = { legend, mediasUrl };
 
     myaxios
       .post("/api/medias", reqBody)
       .then((response) => {
         console.log("The post created ===", response.data);
+        setMediasUrl("");
+        setLegend("");
 
         navigate("/home?tab=medias");
       })
@@ -31,8 +66,11 @@ function AddPostPage() {
     <div className="AddPostPage">
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      <img src={media} alt="media of the post" />
-      <form onSubmit={handleSubmit}>
+      {/* <img src={media} alt="media of the post" /> */}
+      <form onSubmit={handleSubmit} >
+        <label> <img src="/images/change-pic.png" alt="addPhoto"/> Add pictures and/or videos
+            <input type="file" onChange={(e) => handleFilesUpload(e)} multiple/>
+        </label>
         <textarea
           name="legend"
           value={legend}
--- src/services/file-upload.service.js
+++ src/services/file-upload.service.js
@@ -4,9 +4,9 @@ const errorHandler = (err) => {
   throw err;
 };
 
-const uploadImage = (file) => {
+const uploadImage = (files) => {
   return myaxios
-    .post("/api/upload", file)
+    .post("/api/upload", files)
     .then((res) => {
       console.log("res.data ===", res.data);
       return res.data;
